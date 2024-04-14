1e5af09e82464aaf09c4ca04f0663a0f461a0437
Pratham khatri <khatripratham349@gmail.com>
2024-04-15 01:23:25 +0530

    changes done on cloudinary

--- src/controllers/registerUser.js
+++ src/controllers/registerUser.js
@@ -1,13 +1,20 @@
 import {User} from "../models/user.models.js"
 import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
 
-import  {uploadOnCloudinary}  from "../utils/cloudinary.js";
+// import  {uploadOnCloudinary}  from "../utils/cloudinary.js";
 import path from "path";
-import { dirname } from 'path';
+
 import {z} from "zod";
+dotenv.config({
+       path:"./env"
+    
+    })
 
 
 const inputDetails = z.object({
@@ -16,14 +23,40 @@ const inputDetails = z.object({
     email: z.string(),
     password: z.string(),
   });
+  cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
+  
+  
+//   const uploadOnCloudinary=async(localPath)=>{
+//      try{
+//       if (!localPath) {
+//         throw new ApiError(400, "No file is there in local system");
+//       }
+//       const __dirname = import.meta.dirname;
+//       console.log(req.files.avatar[0].path)
+//       const localFilePath = path.resolve(__dirname, "localPath", __filename);
+//       const responce = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto" });
+//       console.log("#######", localPath);
+//        return responce.url; 
+//      } catch (error) {
+//           fs.unlinkSync(localPath)//remove the locally saved temporary file as the upload operation got failed
+//           console.log(error.message)
+//           return null;
+//      }
+  
+  
+//   }
 
 const registerUser=async(req,res)=>{
   
     const { fullName, email, username, password } = req.body;
     // return res.json(new ApiResponse(200,req.files,"user"))
-    const parsedResponce=inputDetails.saveParse(req.body)
-    if(parsedResponce.success){
-        return res.ApiError(411,"give proper input field")
+    const parsedResponce=inputDetails.safeParse(req.body);
+    if(!parsedResponce.success){
+        throw new ApiError(411,"give proper input field")
     }
     console.log("====>",req.files.avatar[0]);
     
@@ -42,6 +75,23 @@ const registerUser=async(req,res)=>{
     if(username===""){
         throw new ApiError(400,"username is required");
     }
+    const uploadOnCloudinary=async(localPath)=>{
+        try{
+         if (!localPath) {
+           throw new ApiError(400, "No file is there in local system");
+         }
+         console.log("3333333",avatarLocalPath)
+         
+         const responce = await cloudinary.uploader.upload(avatarLocalPath, { resource_type: "auto" });
+          return responce.url; 
+        } catch (error) {
+             fs.unlinkSync(localPath)//remove the locally saved temporary file as the upload operation got failed
+             console.log(error.message)
+             return null;
+        }
+     
+     
+     }
 
    const existingUser= await User.findOne({
     $or:[{username},{password}]
